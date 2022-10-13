4311642922e4cdfb48c6b885a3df100b1d3269cd
srgesio <gesioiajm@gmail.com>
2022-10-13 10:09:19 -0300

    sync docs and components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,24 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#75758A',
+      'gray-200': '#E1E1E6',
+      'gray-100': '#C4C4CC',
+
+      'cyan-500': '#81D8F7',
+      'cyan-300': '#9BE1FB'
+    },
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
